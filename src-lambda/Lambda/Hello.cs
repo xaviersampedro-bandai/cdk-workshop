adf33b8aeece58ae609ab7ead95992130909b890
Xavier Sampedro <91746931+xaviersampedro-bandai@users.noreply.github.com>
2021-12-03 13:01:00 +0100

    DotNet hello lambda.

---
+++ src-lambda/Lambda/Hello.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace Lambda
+{
+    public class Hello
+    {
+        public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            context.Logger.Log(request.ToString());
+            
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = 200,
+                Headers = new Dictionary<string, string>
+                {
+                    ["Content-Type"] = "text/plain"
+                },
+                Body = "Hello, CDK! You've hit ${event.path}\n"
+            };
+        }
+    }
+}
